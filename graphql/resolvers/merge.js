edd31f024660a042c7db8a336c8b4f1fdb318161
ralph1786 <ralph1786@gmail.com>
2019-09-18 13:17:15 -0400

    added dataloader for batching and caching

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,6 +1,15 @@
 const Event = require("../../models/event");
 const User = require("../../models/user");
 const { dateToString } = require("../../helpers/date");
+const DataLoader = require("dataloader");
+
+const eventLoader = new DataLoader(eventIds => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoader(userIds => {
+  return User.find({ _id: { $in: userIds } });
+});
 
 const transformEvent = event => {
   return {
@@ -13,11 +22,11 @@ const transformEvent = event => {
 //The following three functions allows graphql resolvers to drill deeper and obtain nested information.
 const user = async userId => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
       _id: user.id,
-      createdEvents: events.bind(this, user._doc.createdEvents)
+      createdEvents: () => eventLoader.loadMany(user._doc.createdEvents)
     };
   } catch (err) {
     throw err;
@@ -26,8 +35,8 @@ const user = async userId => {
 
 const singleEvent = async eventId => {
   try {
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
+    const event = await eventLoader.load(eventId.toString());
+    return event;
   } catch (error) {
     throw error;
   }
@@ -36,6 +45,11 @@ const singleEvent = async eventId => {
 const events = async eventIds => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
+    events.sort((a, b) => {
+      return (
+        eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString())
+      );
+    });
     return events.map(event => {
       return transformEvent(event);
     });
